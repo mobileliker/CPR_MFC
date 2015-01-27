82575e8cb573f5f9851941b42ed28acba337920e
mobileliker <mobileliker@gmail.com>
2015-01-27 15:13:45 +0800

    初始化VS2010 新建MFC项目

---
+++ CPR/CPR/CPR.h
@@ -0,0 +1,39 @@
+
+// CPR.h : CPR 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CCPRApp:
+// 有关此类的实现，请参阅 CPR.cpp
+//
+
+class CCPRApp : public CWinAppEx
+{
+public:
+	CCPRApp();
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCPRApp theApp;
